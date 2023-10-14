f146acbe456e00cd61daed17d339dcc782dc0f66
Abu Bakar Siddique <mohammad.akash31@gmail.com>
2023-10-14 22:49:31 +0600

    Update Routes

--- app/Http/Controllers/IncomeController.php
+++ app/Http/Controllers/IncomeController.php
@@ -6,5 +6,34 @@ use Illuminate\Http\Request;
 
 class IncomeController extends Controller
 {
-    //
+    public function __construct(){
+        $this ->middleware('auth');
+    }
+    public function index(){
+         
+    }
+    public function add(){
+         
+    }
+    public function edit(){
+        
+    }
+    public function view(){
+        
+    }
+    public function insert(){
+        
+    }
+    public function update(){
+        
+    }
+    public function softdelete(){
+       
+    }
+    public function restore(){
+       
+    }
+    public function delete(){
+       
+    }
 }
