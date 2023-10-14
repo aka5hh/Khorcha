9db8815b96ad53685e2445fa595ddd65545aafe9
Abu Bakar Siddique <mohammad.akash31@gmail.com>
2023-10-15 00:21:42 +0600

    updated user files and usercontroller

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,14 +1,16 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-  <head>
+
+<head>
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Admin Panel</title>
     <link rel="stylesheet" href="{{asset('contents/admin')}}/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{asset('contents/admin')}}/css/all.min.css">
     <link rel="stylesheet" href="{{asset('contents/admin')}}/css/style.css">
-  </head>
-  <body>
+</head>
+<body>
     <header>
         <div class="container-fluid header_part">
             <div class="row">
@@ -16,39 +18,42 @@
                 <div class="col-md-7"></div>
                 <div class="col-md-3 top_right_menu text-end">
                     <div class="dropdown">
-                      <button class="btn dropdown-toggle top_right_btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                          <img src="{{asset('contents/admin')}}/images/avatar.png" class="img-fluid">
-                          Saidul Islam Uzzal
-                      </button>
-                      <ul class="dropdown-menu">
-                        <li><a class="dropdown-item" href="#"><i class="fas fa-user-tie"></i> My Profile</a></li>
-                        <li><a class="dropdown-item" href="#"><i class="fas fa-cog"></i> Manage Account</a></li>
-                        <li><a class="dropdown-item" href="#"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
-                      </ul>
+                        <button class="btn dropdown-toggle top_right_btn" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src="{{asset('contents/admin')}}/images/avatar.png" class="img-fluid">
+                            {{Auth::user()->name}}
+                        </button>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#"><i class="fas fa-user-tie"></i> My Profile</a></li>
+                            <li><a class="dropdown-item" href="#"><i class="fas fa-cog"></i> Manage Account</a></li>
+                            <li><a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+                        </ul>
                     </div>
                 </div>
                 <div class="clr"></div>
             </div>
-        </div>  
+        </div>
     </header>
     <section>
         <div class="container-fluid content_part">
             <div class="row">
                 <div class="col-md-2 sidebar_part">
                     <div class="user_part">
-                        <img class="" src="{{asset('contents/admin')}}/images/avatar.png" alt="avatar"/>
-                        <h5>Saidul Islam Uzzal</h5>
+                        <img class="" src="{{asset('contents/admin')}}/images/avatar.png" alt="avatar" />
+                        <h5>{{Auth::user()->name}}</h5>
                         <p><i class="fas fa-circle"></i> Online</p>
                     </div>
                     <div class="menu">
                         <ul>
-                            <li><a href="index.html"><i class="fas fa-home"></i> Dashboard</a></li>
-                            <li><a href="all-user.html"><i class="fas fa-user-circle"></i> Users</a></li>
-                            <li><a href="#"><i class="fas fa-images"></i> Banner</a></li>
+                            <li><a href="{{url('dashboard')}}"><i class="fas fa-home"></i> Dashboard</a></li>
+                            <li><a href="{{url('dashboard/user')}}"><i class="fas fa-user-circle"></i> Users</a></li>
+                            <li><a href="{{url('dashboard/banner')}}"><i class="fas fa-images"></i> Banner</a></li>
                             <li><a href="#"><i class="fas fa-comments"></i> Contact Message</a></li>
                             <li><a href="#"><i class="fas fa-globe"></i> Live Site</a></li>
-                            <li><a href="#"><i class="fas fa-sign-out-alt"></i> Logout</a></li>
+                            <li><a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('frm-logout').submit();"><i class=" fas fa-sign-out-alt"></i> Logout</a></li>
                         </ul>
+                        <form id="frm-logout" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
                     </div>
                 </div>
                 <div class="col-md-10 content">
@@ -57,12 +62,12 @@
                             <div class="bread">
                                 <ul>
                                     <li><a href=""><i class="fas fa-home"></i>Home</a></li>
-                                    <li><a href=""><i class="fas fa-angle-double-right"></i>Dashboard</a></li>                             
+                                    <li><a href=""><i class="fas fa-angle-double-right"></i>Dashboard</a></li>
                                 </ul>
                             </div>
                         </div>
                     </div>
-                    @yield('page')
+                    @yield('content')
                 </div>
             </div>
         </div>
@@ -80,5 +85,6 @@
     </footer>
     <script src="{{asset('contents/admin')}}/js/bootstrap.bundle.min.js"></script>
     <script src="{{asset('contents/admin')}}/js/custom.js"></script>
-  </body>
+</body>
+
 </html>
