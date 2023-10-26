ef45feeeea53eacba1b55739f7de0e8fafc08f20
Abu Bakar Siddique <mohammad.akash31@gmail.com>
2023-10-26 16:50:47 +0600

    Updated ICC and ICC all

--- app/Http/Controllers/IncomeCategoryController.php
+++ app/Http/Controllers/IncomeCategoryController.php
@@ -89,8 +89,19 @@ class IncomeCategoryController extends Controller{
                 return redirect('dashboard/income/category/edit'.$slug);
                 }
     }
-    public function softdelete(){
+    public function softdelete($id){
+        $soft=IncomeCategory::where('incate_status',1)->where('incate_id',$id)->update([
+            'incate_status'=>0,
+            'updated_at'=>Carbon::now()->toDateTimeString(),
+        ]);
 
+        if($soft){
+            Session()->flash('success','Successfully Deleted income category.');
+           return redirect('dashboard/income/category');
+        }else{
+            Session()->flash('error','Opps operation failed.');
+            return redirect('dashboard/income/category');
+            }
     }
     public function restore(){
 
