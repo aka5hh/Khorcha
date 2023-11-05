59955b29445f81df91ba6f3a3164aec740959ad3
Abu Bakar Siddique <mohammad.akash31@gmail.com>
2023-11-05 15:08:14 +0600

    copied all incomecategory files to expensecategory

--- app/Http/Controllers/ExpenseCategoryController.php
+++ app/Http/Controllers/ExpenseCategoryController.php
@@ -2,14 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Session;
+use Illuminate\Support\Facades\Auth;
+use App\Models\ExpenseCategory;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class ExpenseCategoryController extends Controller{
     public function __construct(){
         $this->middleware('auth');
     }
     public function index(){
-         
+        $all=ExpenseCategory::where('expcate_status',1)->orderBy('expcate_id','DESC')->get();
+         return view('admin.expense.category.all', compact('all'));
     }
     public function add(){
          
--- app/Http/Controllers/ExpenseController.php
+++ app/Http/Controllers/ExpenseController.php
@@ -3,6 +3,9 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Expense;
+use Illuminate\Support\Carbon;
+
 
 class ExpenseController extends Controller
 {
